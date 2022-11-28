03ddbb9df60b4262f64dd9e5c2100dfad9a206f8
Pedro Garcia <pedro.metalurgia@gmail.com>
2022-11-27 21:38:10 -0300

    first sentiment analisys

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello")
+from api.vanderSentiment import analysisSentiment
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return render_template('text.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    msgUser = analysisSentiment(text)
+    result = str(msgUser.englishSentiment())
+    #processed_text = text.upper()
+    return result
+
+if __name__ == "__main__":
+    app.run(debug=True)
