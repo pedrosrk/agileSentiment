77163b3bd92dc5b71fbbfc06b36c412de63f9b87
Pedro Saint Clair Garcia <pedro.garcia@take.net>
2023-01-19 20:41:12 -0300

    registration

---
+++ database/db.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="1512",
+  database="agilim"
+)
+
+mycursor = mydb.cursor()
+
+def init_user_dataBase(tableName):
+    mycursor.execute(
+        "CREATE TABLE " + tableName + " (id INT AUTO_INCREMENT PRIMARY KEY,"
+        "name VARCHAR(1255),"
+        "email VARCHAR(1255))"
+        )
+
+def add(val, tableName):
+    sql = "INSERT INTO " + tableName + " (name, email) VALUES (%s, %s)"
+    mycursor.execute(sql, val)
+    mydb.commit()
+
+
+
+'''if __name__ == '__main__':
+    # init_user_dataBase("users")
+    val = ("pedro", "p@net.com")
+    add(val, "users")'''
--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from api.vanderSentiment import analysisSentiment
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
+from database import db
 
 app = Flask(__name__)
 
 @app.route('/')
+def my_home():
+    return render_template('home.html')
+
+@app.route('/', methods=['POST'])
+def my_home_post():
+    name = request.form['name']
+    email = request.form['email']
+    val = (name, email)
+    db.add(val, "users")
+    return redirect('/message'), render_template('text.html', name=name, email=email)
+
+@app.route('/message')
 def my_form():
     return render_template('text.html')
 
-@app.route('/', methods=['POST'])
+@app.route('/message', methods=['POST'])
 def my_form_post():
     text = request.form['text']
     msgUser = analysisSentiment(text)
@@ -15,7 +28,9 @@ def my_form_post():
     pos = score["pos"]
     neg = score["neg"]
     neu = score["neu"]
-    return render_template('home.html', pos=pos, neg=neg, neu=neu ) #"The positivity is " + str(pos) + "<br>The negativity is " + str(neg)
+    return render_template('satisfation.html', pos=pos, neg=neg, neu=neu ) #"The positivity is " + str(pos) + "<br>The negativity is " + str(neg)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    #val = ("rafa", "r@net.com")
+    #db.add(val, "users")
+    app.run(debug=True, host='0.0.0.0') #app.run(debug=True, host='0.0.0.0', port=3000)
