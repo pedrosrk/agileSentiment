a978f91b841a3c4ef00d286fa5cda49c26824a99
Pedro Saint Clair Garcia <pedro.garcia@take.net>
2023-01-27 08:31:05 -0300

    db

--- main.py
+++ main.py
@@ -42,8 +42,6 @@ def my_home():
 def my_home_post():
     name = request.form['name']
     email = request.form['email']
-    #val = (name, email)
-    #db.add(val, "users")
     return render_template('text.html', name=name, email=email)
 
 @app.route('/message')
@@ -58,7 +56,7 @@ def my_form_post():
     pos = score["pos"]
     neg = score["neg"]
     neu = score["neu"]
-    return render_template('satisfation.html', pos=pos, neg=neg, neu=neu ) #"The positivity is " + str(pos) + "<br>The negativity is " + str(neg)
+    return render_template('satisfation.html', pos=pos, neg=neg, neu=neu )
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0') #app.run(debug=True, host='0.0.0.0', port=3000)
+    app.run(debug=True, host='0.0.0.0')
